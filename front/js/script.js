0a2a679818d121f271ff30a3b35317ce24efaa68
naomiacc <fanny.ayello.campos@outlook.com>
2022-07-03 19:27:31 +0200

    refactor : comments and variable renaming

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,4 @@
+// On pointe sur l'élément items du document
 const elementItems = document.getElementById("items");
 
 // Récupération des produits de l'API
@@ -12,7 +13,7 @@ function getProducts() {
     });
 }
 
-// Création des éléments produits dans le DOM
+// Passage de paramètre product entre init et createHTML
 
 init();
 async function init() {
@@ -22,27 +23,34 @@ async function init() {
   });
 }
 
+// Création des éléments produits dans le DOM
+
 function createHTML(product) {
   console.log(product);
+
   //attribut href
   let productLink = document.createElement("a");
   document.querySelector(".items").appendChild(productLink);
   productLink.href = "./product.html?id=" + product._id;
+
   // élément "article"
-  productArticle = document.createElement("article");
+  let productArticle = document.createElement("article");
   productLink.appendChild(productArticle);
+
   // images
-  productImg = document.createElement("img");
+  let productImg = document.createElement("img");
   productArticle.appendChild(productImg);
   productImg.src = product.imageUrl;
   productImg.alt = product.altTxt;
+
   // titre "h3"
-  productName = document.createElement("h3");
+  let productName = document.createElement("h3");
   productArticle.appendChild(productName);
   productName.classList.add("productName");
   productName.innerHTML = product.name;
+
   // description "p"
-  productDescription = document.createElement("p");
+  let productDescription = document.createElement("p");
   productArticle.appendChild(productDescription);
   productDescription.classList.add("productDescription");
   productDescription.innerHTML = product.description;
