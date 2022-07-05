7ebb4fad3e0e1d62133e7efadf9bf7ee90dae18f
naomiacc <fanny.ayello.campos@outlook.com>
2022-07-05 17:00:52 +0200

    feat : add localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -59,7 +59,7 @@ function builProduct(product) {
   }
 }
 
-// Ajout du produit dans le panier
+//Création du ShoppingItem
 class ShoppingItem {
   constructor(id, option, quantity) {
     this.id = id;
@@ -70,28 +70,72 @@ class ShoppingItem {
 const quantity = document.getElementById("quantity");
 const option = document.getElementById("colors");
 
-// création d'un événement sur le boutton "ajouter au panier"
+// Ajout de l'événement sur le bouton "ajouter au panier"
 const addToCart = document.getElementById("addToCart");
 addToCart.addEventListener("click", (event) => {
   event.preventDefault();
   let shoppingItem = {
     quantity: quantity.value,
     option: option.value,
-    _id: id,
+    _id: product.id,
     name: title.textContent,
     price: price.textContent,
-    image: imageUrl,
-    alt: altTxt,
+    image: product.imageUrl,
+    alt: product.altTxt,
   };
 
   localstorage(shoppingItem);
   console.log(shoppingItem);
 });
 
-// Création du localStorage
+//Message d'alerte pour valider l'ajout du produit au panier
+const popupConfirmation = () => {
+  if (window.confirm(` Confirmer l'ajout et voir le panier ou annuler`)) {
+    window.location.href = "cart.html";
+  } else {
+    window.location.href = "index.html";
+  }
+};
+
+//Création du localStorage
 function localstorage(shoppingItem) {
   let shoppingCartLocalStorage = JSON.parse(
     localStorage.getItem("shoppingCart")
   );
   console.log(shoppingCartLocalStorage);
+  // Si produit déjà enregistré
+  if (shoppingCartLocalStorage) {
+    productChecked(shoppingCartLocalStorage, shoppingItem);
+    localStorage.setItem(
+      "shoppingCart",
+      JSON.stringify(shoppingCartLocalStorage)
+    );
+    console.log(shoppingCartLocalStorage);
+    popupConfirmation();
+  } else {
+    // Si aucun produit
+    shoppingCartLocalStorage = [];
+    shoppingCartLocalStorage.push(shoppingItem);
+    localStorage.setItem(
+      "shoppingCart",
+      JSON.stringify(shoppingCartLocalStorage)
+    );
+    console.log(shoppingCartLocalStorage);
+    popupConfirmation();
+  }
+}
+// Fonction pour éviter les doublons dans la panier
+function productChecked(shoppingCartLocalStorage, shoppingItem) {
+  const object = shoppingCartLocalStorage.find(
+    (element) =>
+      element.id === shoppingItem.id && element.option === shoppingItem.option
+  );
+  if (object) {
+    const n = parseInt(object.quantity);
+    const m = parseInt(shoppingItem.quantity);
+    object.quantity = (n + m).toString();
+  } else {
+    shoppingCartLocalStorage.push(shoppingItem);
+  }
+  return shoppingCartLocalStorage;
 }
