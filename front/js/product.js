ae81e6d09eab3a0a73110f351cb2298c6c09f7f7
naomiacc <fanny.ayello.campos@outlook.com>
2022-07-12 19:08:55 +0200

    feat : check color and error message

--- front/js/product.js
+++ front/js/product.js
@@ -14,22 +14,23 @@ function getProductById() {
       console.log(e);
     });
 }
-
 console.log(location);
 
 // Passage de paramètre product entre init et builProduct
-
 init();
 async function init() {
   product = await getProductById();
   builProduct(product);
 
+  //ajout des messages d'erreur si mauvaise saisie de quantité et/ou de couleur
   createErrorMsgHTMLElement();
 
-  // Ajout de l'événement sur le bouton "ajouter au panier"
+  // ajout de l'événement sur le bouton "ajouter au panier" en prenant en compte la vérification de la quantité et de la couleur du produit
   const button = document.getElementById("addToCart");
-  button.addEventListener("click", checkNumber, (event) => {
-    addToCart(event, product);
+  button.addEventListener("click", (event) => {
+    if (checkNumber() && checkColor()) {
+      addToCart(event, product);
+    }
   });
 }
 
@@ -67,7 +68,7 @@ function builProduct(product) {
   }
 }
 
-//Création du ShoppingItem
+//Création du ShoppingItem avec ses éléments
 class ShoppingItem {
   constructor(id, option, quantity) {
     this.id = id;
@@ -75,38 +76,69 @@ class ShoppingItem {
     this.quantity = quantity;
   }
 }
+
+// Vérification de la quantité et de la couleur selectionnée, pour éviter l'ajout d'un produit sans couleur et/ou sans quantité dans le panier
+
 const quantity = document.getElementById("quantity");
 const option = document.getElementById("colors");
 
-// Vérification de la quantité selectionnée
+// On crée une div pour afficher, s'il y a une erreur de saisi, un texte en dessous de l'input#quantity et du selec#colors
 
 function createErrorMsgHTMLElement() {
   let errorElement = document.createElement("div");
   errorElement.setAttribute("id", "error-msg");
   quantity.after(errorElement);
+  document.getElementById("error-msg").style.background = "#FF4500";
+
+  let errorColorElement = document.createElement("div");
+  errorColorElement.setAttribute("id", "error-color");
+  option.after(errorColorElement);
+  document.getElementById("error-color").style.background = "#FF4500";
 }
 
-function checkNumber() {
-  // On suppose que tout est bon, donc on cache les erreurs au début
-  hideError();
-  if (quantity.value > 99) {
-    displayError("too large number");
-  } else if (quantity.value < 1) {
-    displayError("too small number");
+// on vérifie la couleur
+function checkColor() {
+  // on suppose que tout est bon, donc on cache les erreurs au début
+  hideColorError();
+  if (option.value == "") {
+    displayColorError("Veuillez choisir une couleur");
+  } else if (option.value.length > 1) {
+    return true;
   }
 }
 
+// on vérifie la quantité
+function checkNumber() {
+  // on suppose que tout est bon, donc on cache les erreurs au début
+  hideError();
+  if (quantity.value > 99) {
+    displayError("Veuillez séléctionner une quantité entre 1 et 99");
+  } else if (quantity.value < 1) {
+    displayError("Veuillez séléctionner une quantité entre 1 et 99");
+  } else if (quantity.value > 0 && quantity.value < 100) {
+    return true;
+  }
+}
+
+// Fonctions paramètres pour mettre en exécution les messages d'erreur
 function displayError(msg) {
   let errorElement = document.getElementById("error-msg");
   errorElement.innerText = msg;
 }
-
+function displayColorError(msg) {
+  let errorColorElement = document.getElementById("error-color");
+  errorColorElement.innerText = msg;
+}
 function hideError() {
   let errorElement = document.getElementById("error-msg");
   errorElement.innerText = "";
 }
+function hideColorError() {
+  let errorColorElement = document.getElementById("error-color");
+  errorColorElement.innerText = "";
+}
 
-// Fonction ajout au panier
+// Fonction ajout au panier, on envoie les caractéristiques du produit
 function addToCart(event, product) {
   event.preventDefault();
   let shoppingItem = {
@@ -121,7 +153,7 @@ function addToCart(event, product) {
   console.log(shoppingItem);
 }
 
-//Message d'alerte pour valider l'ajout du produit au panier
+//Message d'alerte confirmant l'ajout du produit dans le panier
 const popupConfirmation = () => {
   window.alert(`Le produit a été ajouté au panier`);
 };
@@ -135,9 +167,10 @@ function saveDataLS(shoppingItem) {
 
   // Si produit déjà enregistré dans le panier
   if (shoppingCartLocalStorage) {
+    console.log("je suis déjà dans le panier");
     let result = productChecked(shoppingCartLocalStorage, shoppingItem);
     localStorage.setItem("shoppingCart", JSON.stringify(result));
-    console.log(result);
+    console.log("result", result);
     popupConfirmation();
   }
 
@@ -157,18 +190,24 @@ function saveDataLS(shoppingItem) {
   }
 }
 
-// Fonction pour éviter les doublons dans la panier
+// Fonction pour éviter les doublons dans la panier : on appelle les variables à comparer
 function productChecked(shoppingCartLocalStorage, shoppingItem) {
+  // On recherche et vérifie si les deux variables ont le même id et la même option
   const object = shoppingCartLocalStorage.find(
     (element) =>
-      element.id === shoppingItem.id && element.option === shoppingItem.option
+      element._id === shoppingItem._id && element.option === shoppingItem.option
   );
+  console.log("objet", object);
+
+  // s'il s'agit du même "object" alors on rectifie la quantité
   if (object) {
     const n = parseInt(object.quantity);
     const m = parseInt(shoppingItem.quantity);
     object.quantity = n + m;
+
+    // sinon, on pousse le nouvel élément dans le LocalStorage
   } else {
-    shoppingCartLocalStorage.push(object);
+    shoppingCartLocalStorage.push(shoppingItem);
   }
   return shoppingCartLocalStorage;
 }
