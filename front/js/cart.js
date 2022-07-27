36209c0001c01e27fbb09e071ac312987472f8cf
naomiacc <fanny.ayello.campos@outlook.com>
2022-07-27 13:50:37 +0200

    feat : functions total quantity and total price

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,88 +2,95 @@
 let shoppingCartLocalStorage = JSON.parse(localStorage.getItem("shoppingCart"));
 console.log(shoppingCartLocalStorage);
 
-// Création tableau panier complet
-let panierComplet = [];
+// Fonction pour récupérer les produits du LocalStorage
+async function getProduct() {
+  // on créé un tableau pour constituer le panier complet
+  let panierComplet = [];
 
-// Récupération des produits dans le LocalStorage
-function getProduct() {
+  // la boucle attend que le fetch soit fini pour chaque tour de boucle
   for (i = 0; i < shoppingCartLocalStorage.length; i++) {
-    fetch(
+    await fetch(
       "http://localhost:3000/api/products/" + shoppingCartLocalStorage[i]._id
     )
       .then(function (res) {
         return res.json();
       })
       .then((products) => {
-        console.log(i);
+        //console.log(i);
+        //console.log(products);
+        // console.log(shoppingCartLocalStorage[i]); //
+
+        // on créé un objet comporant les propriétés et les valeurs nécessaires pour consituer le panier
         const obj = {
           _id: products._id,
           name: products.name,
           price: products.price,
-          color: shoppingCartLocalStorage[i - 1].option,
-          quantity: shoppingCartLocalStorage[i - 1].quantity,
+          color: shoppingCartLocalStorage[i].option,
+          quantity: shoppingCartLocalStorage[i].quantity,
           alt: products.altTxt,
-          img: shoppingCartLocalStorage[i - 1].image,
+          img: shoppingCartLocalStorage[i].image,
         };
+        // on pousse l'objet dans le tableau créé
         panierComplet.push(obj);
+        // console.log(panierComplet); //
       })
       .catch(function (error) {
         console.log(error);
       });
   }
+  return panierComplet;
 }
 
 // Initialisation des fonctions
 init();
 async function init() {
-  await getProduct();
-  createHTMLBasket();
+  let panierComplet = await getProduct();
+  createHTMLBasket(panierComplet);
 
   AddEventChangeQuantity();
   AddEventRemoveQuantity();
+
+  calculTotalQuantity();
+  calculTotalPrice();
 }
 
-// Création des éléments à afficher dans le panier
-function createHTMLBasket(products) {
+// Fonction pour créer le HTML du panier dans lequel sont insérés les propriétés du tableau "Panier Complet"
+function createHTMLBasket(panierComplet) {
   const shoppingItem = document.getElementById(`cart__items`);
 
-  panierComplet.map((product) => {
-    console.log(product);
+  console.log("complet", panierComplet);
 
-    listItem.innerHTML = `
-<article class="cart__item" data-id=${shoppingCartLocalStorage[i]._id} data-color="${shoppingCartLocalStorage[i].option}">
-    <div class="cart__item__img">
-        <img src=${shoppingCartLocalStorage[i].image} alt=${shoppingCartLocalStorage[i].alt}>
-    </div>
-    <div class="cart__item__content">
-        <div class="cart__item__content__titlePrice">
-            <h2>${shoppingCartLocalStorage[i].name}</h2>
-            <p>${shoppingCartLocalStorage[i].price} €</p>
+  panierComplet.map((product) => {
+    shoppingItem.innerHTML += `
+    <article class="cart__item" data-id="${product._id}" data-color="${product.color}">
+        <div class="cart__item__img">
+            <img src="${product.img}" alt="${product.alt}">
         </div>
-        <div class="cart__item__content__settings">
-            <p>Couleur : ${shoppingCartLocalStorage[i].option}</p>
-                <div class="cart__item__content__settings__quantity">
-                <p>Qté :  </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${shoppingCartLocalStorage[i].quantity}">
-                </div>
+        <div class="cart__item__content">
+            <div class="cart__item__content__titlePrice">
+                <h2>${product.name}</h2>
+                <p>${product.price}€</p>
+            </div>
+            <div class="cart__item__content__settings">
+                <p>Couleur : ${product.color}</p>
+                    <div class="cart__item__content__settings__quantity">
+                    <p>Qté :  </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                    </div>
+            </div>
+            <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+            </div>
         </div>
-        <div class="cart__item__content__settings__delete">
-            <p class="deleteItem">Supprimer</p>
-        </div>
-    </div>
-</article>
+    </article>
     `;
   });
-  shoppingItem.appendChild(listItem);
-  console.log(listItem);
 }
 
-// Stockage des éléments dans des tableaux
-let deleteItemContainer = [...document.getElementsByClassName("deleteItem")];
-let quantityContainer = [...document.getElementsByClassName("itemQuantity")];
-
-// Suppression d'un produit
+// Fonction pour supprimer un produit dans le panier
 function AddEventRemoveQuantity() {
+  let deleteItemContainer = [...document.getElementsByClassName("deleteItem")];
+
   deleteItemContainer.forEach((item, index) => {
     item.addEventListener("click", () => {
       // Dans le DOM
@@ -100,8 +107,10 @@ function AddEventRemoveQuantity() {
   });
 }
 
-// Modification de la quantité
+// Fonction pour modifier la quantité d'un produit dans le panier
 function AddEventChangeQuantity() {
+  let quantityContainer = [...document.getElementsByClassName("itemQuantity")];
+
   quantityContainer.forEach((shoppingItem, index) => {
     shoppingItem.addEventListener("change", () => {
       // Au click, modifie l'objet sur le LocalStorage et le dom
@@ -115,22 +124,25 @@ function AddEventChangeQuantity() {
   });
 }
 
-// Totalisation des quantités et de la somme finale des articles
-let sumShoppingItem = 0;
-let sumPrice = 0;
-let totalQuantity = document.getElementById("totalQuantity");
-let totalPrice = document.getElementById("totalPrice");
-
-if (shoppingCartLocalStorage !== null) {
+// Fonction pour calculer le nombre de produit total dans le panier
+function calculTotalQuantity() {
+  let number = 0;
   for (let j = 0; j < shoppingCartLocalStorage.length; j++) {
     let quantityLoop = parseInt(shoppingCartLocalStorage[j].quantity);
-    let priceLoop = parseInt(shoppingCartLocalStorage[j].price);
-    sumShoppingItem += quantityLoop;
-    sumPrice += priceLoop * quantityLoop;
+    number += quantityLoop;
   }
+  let totalQuantity = document.getElementById("totalQuantity");
+  totalQuantity.innerText = number;
 }
 
-if (totalQuantity && totalPrice) {
-  totalQuantity.innerHTML = sumShoppingItem;
-  totalPrice.innerHTML = sumPrice;
+// Fonction pour calculer le prix total du panier
+async function calculTotalPrice() {
+  const shoppingItem = await getProduct();
+  let totalPrice = 0;
+  for (let product of shoppingItem) {
+    const shoppingItem = getProduct();
+    totalPrice += product.quantity * product.price;
+  }
+  let totalPriceCart = document.getElementById("totalPrice");
+  totalPriceCart.innerText = totalPrice;
 }
